b2297816b42835db173b415229f6c274513425ba
AlekseyDrobnyi <Drobnyi_leh@mail.ru>
2023-05-25 22:13:53 +0500

    15.1

---
+++ 15.1/.tf/main.tf
@@ -0,0 +1,49 @@
+provider "yandex" {
+  cloud_id  = "b1gkd8ratktloashio3p"
+  folder_id = "b1g3oeejfshbl9k1707m"
+  zone      = "ru-central1-a"
+}
+
+
+resource "yandex_compute_instance" "vm-1" {
+  name = "terraform1"
+
+  resources {
+    cores  = 2
+    memory = 2
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id = "fd89ovh4ticpo40dkbvd"
+	  name        = "root-vm1"
+      type        = "network-nvme"
+      size        = "40"
+    }
+  }
+
+  network_interface {
+    subnet_id = yandex_vpc_subnet.subnet-1.id
+    nat       = true
+  }
+  
+}
+
+resource "yandex_vpc_network" "network-1" {
+  name = "network1"
+}
+
+resource "yandex_vpc_subnet" "subnet-1" {
+  name           = "subnet1"
+  zone           = "ru-central1-a"
+  network_id     = yandex_vpc_network.network-1.id
+  v4_cidr_blocks = ["192.168.10.0/24"]
+}
+
+output "internal_ip_address_vm_1" {
+  value = yandex_compute_instance.vm-1.network_interface.0.ip_address
+}
+
+output "external_ip_address_vm_1" {
+  value = yandex_compute_instance.vm-1.network_interface.0.nat_ip_address
+}
---
+++ 15.1/.tf/vars.tf
@@ -0,0 +1,17 @@
+variable "yc_token" {
+  default = "AQAAAAAn9jyAAATuwcPbE16Eqk2mtER-C-YqACc"
+}
+
+variable "yc_cloud_id" {
+  default = "b1gkd8ratktloashio3p"
+}  
+ 
+ 
+variable "yc_folder-id" {
+  default = "b1g6ni2modvgo3v2jaqp"
+}  
+ 
+variable "yc_region" {
+  default = "ru-central1-a"
+}  
+
