7ae21c86be4376a4a42c23aec0386d3bff5ccc35
AlekseyDrobnyi <Drobnyi_leh@mail.ru>
2023-05-28 15:11:51 +0500

    15.2

---
+++ 15.2/.tf/vars.tf
@@ -0,0 +1,21 @@
+variable "yc_token" {
+  default = "AQAAAAAn9jyAAATuwcPbE16Eqk2mtER-C-YqACc"
+}
+
+variable "yc_cloud_id" {
+  default = "b1gkd8ratktloashio3p"
+}  
+ 
+ 
+variable "yc_folder_id" {
+  default = "b1gm8c498m5ceggcmcga"
+}  
+ 
+variable "yc_region" {
+  default = "ru-central1-a"
+}  
+ 
+variable "my_picture" {
+  default = "picture.png"
+}  
+
